557dc59c9c967da13c76e529b8928bcdb3b5db93
sowji <jujjurisowjanya@gmail.com>
2023-12-20 18:35:33 +0530

    new file

--- roboshop.sh
+++ roboshop.sh
@@ -17,14 +17,14 @@ echo "$i:$IP_ADDRESS"
 aws route53 change-resource-record-sets \  --hosted-zone-id $ZONE_ID  \  --change-batch '
 { 
     "Comment": "Testing creating a record set" 
-      ,"Changes": [ { 
-    ,"Action": "CREATE"
+      , "Changes": [ { 
+    , "Action": "CREATE"
     "ResourceRecordSet": 
     { 
         "Name": " '$i'.'$DOMAIN_NAME'"
-         ,"Type": "A" 
-         ,"TTL": 1
-          ,"ResourceRecords": [ { 
+         , "Type": "A" 
+         , "TTL": 1
+          , "ResourceRecords": [ { 
             "Value": "'$IP_ADDRESS'"
              } ]
               }
